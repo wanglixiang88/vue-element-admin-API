13fc6131139e3a25517f38390f6c43dc92369972
wanglixiang <codegirl_wanglixiang@outlook.com>
2020-10-15 15:49:53 +0800

    增加数据库模型

---
+++ dataModel/sys_user.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vueElementAdmin.Model
+{
+    class sys_user
+    {
+        /// <summary>
+        /// 用户ID guid 唯一
+        /// </summary>
+        public string userId { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string userName { get; set; }
+
+        /// <summary>
+        /// 密码 MD5加密
+        /// </summary>
+        public string passWord { get; set; }
+
+        /// <summary>
+        /// 用户token
+        /// </summary>
+        public string userToken { get; set; }
+
+        /// <summary>
+        /// 是否无效 0.有效 1.无效
+        /// </summary>
+        public int isValid { get; set; }
+
+        /// <summary>
+        /// 创建人ID
+        /// </summary>
+        public string createUserId { get; set; }
+
+        /// <summary>
+        /// 创建人姓名
+        /// </summary>
+        public string createUserName { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime createTime { get; set; }
+
+        /// <summary>
+        /// 更新人ID
+        /// </summary>
+        public string updateUserId { get; set; }
+
+        /// <summary>
+        /// 更新人名称
+        /// </summary>
+        public string updateUserName { get; set; }
+
+        /// <summary>
+        ///更新时间
+        /// </summary>
+        public DateTime? updateTime { get; set; }
+    }
+}
