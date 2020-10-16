be21ebe38083e46b18afe2602a96dbd6dd8cc15e
wanglixiang <codegirl_wanglixiang@outlook.com>
2020-10-16 11:55:51 +0800

    添加项目

---
+++ IVueElememtAdminRepository/ISysUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vueElementAdmin.Model;
+
+namespace IVueElememtAdminRepository
+{
+    public interface ISysUserRepository
+    {
+        sys_user GetUserInfoByToken(string token);
+    }
+}
---
+++ VueElememtAdminRepository/SysUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using IVueElememtAdminRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vueElementAdmin.Model;
+
+namespace VueElememtAdminRepository
+{
+    public class SysUserRepository: ISysUserRepository
+    {
+        public sys_user GetUserInfoByToken(string token)
+        {
+            return null;
+        }
+    }
+}
